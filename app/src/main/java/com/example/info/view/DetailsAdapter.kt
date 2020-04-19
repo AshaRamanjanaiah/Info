86875996a3b4341f0ea0b9391d1905a01ea2f81d
Asha Ramanjanaiah <asha.ramanjanaiah@gmail.com>
2020-04-19 13:09:15 +1000

    - Polishing UI - Added Data binding to reduce boilerplate code in activity

--- app/src/main/java/com/example/info/view/DetailsAdapter.kt
+++ app/src/main/java/com/example/info/view/DetailsAdapter.kt
@@ -1,12 +1,12 @@
 package com.example.info.view
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.info.R
+import com.example.info.databinding.ItemDetailsBinding
 import com.example.info.model.Details
-import kotlinx.android.synthetic.main.item_details.view.*
 
 class DetailsAdapter(private val detailsList: ArrayList<Details>):
     RecyclerView.Adapter<DetailsAdapter.DetailsViewHolder>() {
@@ -19,16 +19,16 @@ class DetailsAdapter(private val detailsList: ArrayList<Details>):
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailsViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_details, parent, false)
+        val view = DataBindingUtil.inflate<ItemDetailsBinding>(inflater,
+            R.layout.item_details, parent, false)
         return DetailsViewHolder(view)
     }
 
     override fun getItemCount(): Int = detailsList.size
 
     override fun onBindViewHolder(holder: DetailsViewHolder, position: Int) {
-        holder.view.title.text = detailsList[position].title
-        holder.view.description.text = detailsList[position].description
+        holder.view.details = detailsList[position]
     }
 
-    class DetailsViewHolder(var view: View): RecyclerView.ViewHolder(view)
+    class DetailsViewHolder(var view: ItemDetailsBinding): RecyclerView.ViewHolder(view.root)
 }
